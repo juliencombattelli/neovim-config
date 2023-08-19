736c4304aa31f4fae1cfe9fc17adf2112e380c80
Julien Combattelli <julien.combattelli@gmail.com>
2023-08-19 12:25:43 +0200

    Move profile config into init.lua

--- lua/jucom/init.lua
+++ lua/jucom/init.lua
@@ -1,4 +1,9 @@
-jucom = {}
+jucom = {
+  -- Codedark color scheme definition with some adaptation to make it closer to Modern Dark VSCode theme
+  colorscheme = "codedark",
+  -- Force lualine theme
+  lualine = require("jucom.lualine.jucom-codedark"),
+}
 
 require("common.lazy-bootstrap")
 require("lazy").setup("jucom.plugins")
