f08d7de2707b1e879b9fbb734f1a342720a5dc9c
Julien Combattelli <julien.combattelli@gmail.com>
2023-08-21 20:31:45 +0200

    Add colorizer

--- lua/jucom/plugins/git-blame.lua
+++ lua/jucom/plugins/git-blame.lua
@@ -1,5 +1,8 @@
 vim.g.gitblame_highlight_group = "NonText"
 
+-- Must be higher than colorizer priority (not implemented upstream for now)
+vim.g.gitblame_set_extmark_options = { priority = 100 }
+
 return {
   {
     "f-person/git-blame.nvim",
