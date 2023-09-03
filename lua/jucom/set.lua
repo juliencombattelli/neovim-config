e711db438f2279f1454427786ac738c629ed4f77
Julien Combattelli <julien.combattelli@gmail.com>
2023-09-03 09:33:08 +0200

    Fix Wsl clipboard setting

--- lua/jucom/set.lua
+++ lua/jucom/set.lua
@@ -62,17 +62,17 @@ vim.opt.colorcolumn = "80,100,120"
 
 -- Enable system clipboard on WSL
 -- Refer to https://neovim.io/doc/user/provider.html#clipboard-wsl
-if vim.fn.has("wsl") then
-    vim.cmd[[ let g:clipboard = {
-        \   'name': 'WslClipboard',
-        \   'copy': {
-        \      '+': 'clip.exe',
-        \      '*': 'clip.exe',
-        \    },
-        \   'paste': {
-        \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        \   },
-        \   'cache_enabled': 0,
-        \ } ]]
+if vim.fn.has("wsl") == "0" then
+  vim.cmd[[ let g:clipboard = {
+    \   'name': 'WslClipboard',
+    \   'copy': {
+    \      '+': 'clip.exe',
+    \      '*': 'clip.exe',
+    \    },
+    \   'paste': {
+    \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    \   },
+    \   'cache_enabled': 0,
+    \ } ]]
 end
