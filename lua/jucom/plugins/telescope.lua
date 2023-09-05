8156206646a007986c1167eb9b970d04705ddb1f
Julien Combattelli <julien.combattelli@gmail.com>
2023-09-05 14:18:21 +0200

    Use vertical layout as default for Telescope

--- lua/jucom/plugins/telescope.lua
+++ lua/jucom/plugins/telescope.lua
@@ -1,4 +1,9 @@
 return {
   "nvim-telescope/telescope.nvim", branch = "0.1.x",
+  opts = {
+    defaults = {
+      layout_strategy = "vertical"
+    },
+  },
   dependencies = { "nvim-lua/plenary.nvim" }
 }
